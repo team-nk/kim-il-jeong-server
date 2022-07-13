5c882d5192b02f7ac6aa0484c9fdbae78e04cd0f
jaemin <sonjm3518@dsm.hs.kr>
2022-07-13 11:09:02 +0900

    📑 :: (#2) main

---
+++ kim-il-jeong-server/kimiljeong-infrastructure/src/main/java/com/nk/kimiljeongserver/KimIlJeongServerApplication.java
@@ -0,0 +1,13 @@
+package com.nk.kimiljeongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimIlJeongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimIlJeongServerApplication.class, args);
+    }
+
+}
