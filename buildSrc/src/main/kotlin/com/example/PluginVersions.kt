f18030540550505ec1376f291d97927e160b9add
jaemin <sonjm3518@dsm.hs.kr>
2022-08-21 23:58:19 +0900

    📑 :: PluginVersions

---
+++ buildSrc/src/main/kotlin/com/example/PluginVersions.kt
@@ -0,0 +1,7 @@
+object PluginVersions {
+    const val SPRING_BOOT_FRAMEWORK_VERSION = "2.7.2"
+    const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.0.12.RELEASE"
+    const val JVM_VERSION = "1.6.21"
+    const val PLUGIN_SPRING_VERSION = "1.6.21"
+    const val PLUGIN_JPA_VERSION = "1.6.21"
+}
