b044bbf8ec475f914273b29db70f199c0d171858
jaemin <sonjm3518@dsm.hs.kr>
2022-08-22 11:39:33 +0900

    refactor :: dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,49 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.2"
-    id("io.spring.dependency-management") version "1.0.12.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    id("org.springframework.boot") version PluginVersions.SPRING_BOOT_FRAMEWORK_VERSION
+    id("io.spring.dependency-management") version PluginVersions.SPRING_DEPENDENCY_MANAGEMENT_VERSION
+    kotlin("jvm") version PluginVersions.JVM_VERSION
+    kotlin("plugin.spring") version PluginVersions.PLUGIN_SPRING_VERSION
+    kotlin("plugin.jpa") version PluginVersions.PLUGIN_JPA_VERSION
 }
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom(Dependency.CLOUD_AWS_DEPENDENCIES)
+    }
+}
+
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation(Dependency.R2DBC)
+    implementation(Dependency.VALIDATION)
+    implementation(Dependency.WEB)
+    implementation(Dependency.JACKSON)
+    implementation(Dependency.JPA)
+    implementation(Dependency.Reactor.KOTLIN_EXTENSIONS)
+    implementation(Dependency.REFLECT)
+    implementation(Dependency.STDLIB_JDK8)
+    implementation(Dependency.Reactor.KOTLINX_COROUTINES)
+    implementation(Dependency.SPRING_SECURITY)
+    implementation(Dependency.REDIS_REACTIVE)
+    implementation(Dependency.CLOUD_AWS)
+    implementation(Dependency.OAUTH2)
+    testImplementation(Dependency.Reactor.TEST)
+    testImplementation(Dependency.TEST)
 }
 
 tasks.withType<KotlinCompile> {
@@ -31,4 +55,5 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+
 }
