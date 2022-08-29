4ef74398f0c7f86e49f56218c241379ea3e37140
jaemin <sonjm3518@dsm.hs.kr>
2022-08-29 22:33:03 +0900

    feat :: ksp 추가

--- buildSrc/src/main/kotlin/com/example/PluginVersions.kt
+++ buildSrc/src/main/kotlin/com/example/PluginVersions.kt
@@ -1,7 +1,8 @@
 object PluginVersions {
-    const val SPRING_BOOT_FRAMEWORK_VERSION = "2.7.2"
+    const val SPRING_BOOT_VERSION = "2.7.2"
     const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.0.12.RELEASE"
     const val JVM_VERSION = "1.6.21"
-    const val PLUGIN_SPRING_VERSION = "1.6.21"
-    const val PLUGIN_JPA_VERSION = "1.6.21"
+    const val SPRING_PLUGIN_VERSION = "1.6.21"
+    const val JPA_PLUGIN_VERSION = "1.6.21"
+    const val KSP_VERSION = "1.6.21"
 }
