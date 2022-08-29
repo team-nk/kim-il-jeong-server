f41204aec801cea6c96099b6e213dc9773277c9f
jaemin <sonjm3518@dsm.hs.kr>
2022-08-29 17:36:23 +0900

    perf :: dependencies 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,59 +1,42 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    id("org.springframework.boot") version PluginVersions.SPRING_BOOT_FRAMEWORK_VERSION
-    id("io.spring.dependency-management") version PluginVersions.SPRING_DEPENDENCY_MANAGEMENT_VERSION
     kotlin("jvm") version PluginVersions.JVM_VERSION
-    kotlin("plugin.spring") version PluginVersions.PLUGIN_SPRING_VERSION
-    kotlin("plugin.jpa") version PluginVersions.PLUGIN_JPA_VERSION
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        version = PluginVersions.JVM_VERSION
+    }
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
+    dependencies {
+        implementation(Dependencies.REFLECT)
+        implementation(Dependencies.JDK)
+        implementation(Dependencies.TEST)
     }
 }
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    group = "com.example"
+    version = "0.0.1-SNAPSHOT"
 
-dependencyManagement {
-    imports {
-        mavenBom(Dependency.CLOUD_AWS_DEPENDENCIES)
+    tasks {
+        compileKotlin {
+            kotlinOptions {
+                freeCompilerArgs = listOf("-Xjsr305=strict")
+                jvmTarget = "17"
+            }
+        }
+
+        compileJava {
+            sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+        }
+    }
+
+    repositories {
+        mavenCentral()
     }
 }
 
-dependencies {
-    implementation(Dependency.R2DBC)
-    implementation(Dependency.VALIDATION)
-    implementation(Dependency.WEB)
-    implementation(Dependency.JACKSON)
-    implementation(Dependency.JPA)
-    implementation(Dependency.Reactor.KOTLIN_EXTENSIONS)
-    implementation(Dependency.REFLECT)
-    implementation(Dependency.STDLIB_JDK8)
-    implementation(Dependency.Reactor.KOTLINX_COROUTINES)
-    implementation(Dependency.SPRING_SECURITY)
-    implementation(Dependency.REDIS_REACTIVE)
-    implementation(Dependency.CLOUD_AWS)
-    implementation(Dependency.OAUTH2)
-    testImplementation(Dependency.Reactor.TEST)
-    testImplementation(Dependency.TEST)
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "17"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-
+tasks.getByName<Jar>("jar") {
+    enabled = false
 }
