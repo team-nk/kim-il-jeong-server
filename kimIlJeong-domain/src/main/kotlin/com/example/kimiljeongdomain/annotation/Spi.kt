bb36988690ac04835aeadabada1410f24f6c0e92
jaemin <sonjm3518@dsm.hs.kr>
2022-09-02 14:11:10 +0900

    feat :: Spi

---
+++ kimIlJeong-domain/src/main/kotlin/com/example/kimiljeongdomain/annotation/Spi.kt
@@ -0,0 +1,8 @@
+package com.example.kimiljeongdomain.annotation
+
+import java.lang.annotation.Inherited
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Inherited
+annotation class Spi
