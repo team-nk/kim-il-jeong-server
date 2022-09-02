368fbe9c09f536fd43ac57dd62fb840913a05027
jaemin <sonjm3518@dsm.hs.kr>
2022-09-02 14:11:30 +0900

    feat :: BaseUUIDEntity

---
+++ kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/entity/BaseUUIDEntity.kt
@@ -0,0 +1,18 @@
+package com.example.kimiljeongserver.global.entity
+
+import org.hibernate.annotations.GenericGenerator
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+abstract class BaseUUIDEntity(id: UUID) {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    private val id: UUID = UUID(0, 0)
+}
