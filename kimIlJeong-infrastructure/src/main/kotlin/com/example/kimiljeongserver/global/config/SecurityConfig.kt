75a86e716b2adc077fddf9b3fe525d7b6ae2d1d8
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 23:44:01 +0900

    project :: 기본 Security 설정

--- kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/config/SecurityConfig.kt
+++ kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/config/SecurityConfig.kt
@@ -1,29 +1,35 @@
 package com.example.kimiljeongserver.global.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfiguration() {
+class SecurityConfig {
 
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
-                .formLogin().disable()
-                .csrf().disable()
-                .cors()
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
         http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         http
-                .authorizeRequests()
-                .antMatchers().permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .apply(FilterConfig())
+            .authorizeRequests()
+            .anyRequest().authenticated()
+        http
+            .apply(FilterConfig())
 
+        return http.build()
     }
+
+    @Bean
+    fun passwordEncoder() = BCryptPasswordEncoder()
 }
