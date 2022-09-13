aed9693e9736b60427f306cfc39426fce3a14faf
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 23:40:47 +0900

    refactor :: if else → when

--- kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/error/ErrorHandlingFilter.kt
+++ kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/error/ErrorHandlingFilter.kt
@@ -1,34 +1,37 @@
 package com.example.kimiljeongserver.global.error
 
-import com.example.kimiljeongdomain.error.ErrorCode
+import com.cheajib.cheajibserver.global.error.exception.InternalServerError
 import com.example.kimiljeongdomain.error.KimIlJeongException
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.MediaType
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class ErrorHandlingFilter : OncePerRequestFilter() {
-
-    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+class ErrorHandlingFilter(
+    private val objectMapper: ObjectMapper
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
         try {
             filterChain.doFilter(request, response)
-        } catch (e: KimIlJeongException) {
-            errorToJson(e.errorCode, response)
         } catch (e: Exception) {
-            if (e.cause is KimIlJeongException) {
-                errorToJson((e.cause as KimIlJeongException).errorCode, response)
-            } else {
-                e.printStackTrace()
-                errorToJson(ErrorCode.INVALID_SERVER_ERROR, response)
+            when (e) {
+                is KimIlJeongException -> writeErrorCode(e, response)
+                else -> writeErrorCode(InternalServerError.EXCEPTION, response)
             }
         }
     }
 
-    private fun errorToJson(errorCode: ErrorCode, response: HttpServletResponse) {
-        response.status = errorCode.status
+    private fun writeErrorCode(exception: KimIlJeongException, response: HttpServletResponse) {
+        val errorResponse = ErrorResponse.of(exception)
+
+        response.status = errorResponse.errorStatus
         response.contentType = MediaType.APPLICATION_JSON_VALUE
-        response.writer.write(ErrorResponse(errorCode).toString())
+        response.writer.write(objectMapper.writeValueAsString(errorResponse))
     }
 }
-
