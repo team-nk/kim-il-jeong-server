c9d18e89f11ad43bee42f78e7758f1843aa91574
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 23:40:12 +0900

    refactor :: content 추가

--- kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/error/ErrorResponse.kt
+++ kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/error/ErrorResponse.kt
@@ -1,18 +1,33 @@
 package com.example.kimiljeongserver.global.error
 
-import com.example.kimiljeongdomain.error.ErrorCode
+import com.example.kimiljeongdomain.error.KimIlJeongException
+import com.fasterxml.jackson.annotation.JsonInclude
 
-class ErrorResponse(errorCode: ErrorCode) {
+class ErrorResponse<T>(
+    val errorStatus: Int,
+    val errorCode: String,
+    val errorMessage: String,
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    val content: T?
+) {
 
-    private val status: Int = errorCode.status
-    private val code: String = errorCode.code
-    private val message: String = errorCode.message
+    companion object {
+        fun of(exception: KimIlJeongException): ErrorResponse<Unit> {
+            return ErrorResponse(
+                errorStatus = exception.errorStatus,
+                errorCode = exception.errorCode,
+                errorMessage = exception.errorMessage,
+                content = null
+            )
+        }
+    }
 
     override fun toString(): String {
         return "{\n" +
-                "\t\"status\": " + status +
-                ",\t\"code\": \"" + code + '\"' +
-                ",\t\"message\": \"" + message + '\"' +
-                "\n}";
+                "\t\"status\": " + errorStatus +
+                ",\t\"code\": \"" + errorCode + '\"' +
+                ",\t\"message\": \"" + errorMessage + '\"' +
+                ",\t\"content\": \"" + content + '\"' +
+                "\n}"
     }
 }
