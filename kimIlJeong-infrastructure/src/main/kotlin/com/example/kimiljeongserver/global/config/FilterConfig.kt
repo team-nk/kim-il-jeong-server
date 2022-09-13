386abcbc4a2f69496602df834c36e43c58672024
jaemin <sonjm3518@dsm.hs.kr>
2022-09-13 23:42:03 +0900

    refactor :: objectMapper 사용

--- kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/config/FilterConfig.kt
+++ kimIlJeong-infrastructure/src/main/kotlin/com/example/kimiljeongserver/global/config/FilterConfig.kt
@@ -1,15 +1,21 @@
 package com.example.kimiljeongserver.global.config
 
 import com.example.kimiljeongserver.global.error.ErrorHandlingFilter
+import com.example.kimiljeongserver.global.security.jwt.JwtTokenFilter
+import com.example.kimiljeongserver.global.security.jwt.JwtTokenParser
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.DefaultSecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
-class FilterConfig : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+class FilterConfig(
+    private val jwtTokenParser: JwtTokenParser,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
 
     override fun configure(builder: HttpSecurity) {
-        builder.addFilterBefore(AuthenticationFilter(), UsernamePasswordAuthenticationFilter::class.java)
-        builder.addFilterBefore(ErrorHandlingFilter(), AuthenticationFilter::class.java)
+        builder.addFilterBefore(JwtTokenFilter(jwtTokenParser), UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(ErrorHandlingFilter(objectMapper), JwtTokenFilter::class.java)
     }
 }
