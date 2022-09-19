1bae1839ec0b4b5d1452b4df201f85a073bb4d29
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-19 15:20:04 +0900

    feat: global error code

---
+++ src/main/kotlin/team/nk/kimiljeongserver/global/error/GlobalErrorCode.kt
@@ -0,0 +1,16 @@
+package team.nk.kimiljeongserver.global.error
+
+import org.springframework.http.HttpStatus
+
+enum class GlobalErrorCode(
+    private val status: HttpStatus,
+    private val message: String
+) : ErrorProperty {
+
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
+
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+
+    override fun status() = status.value()
+    override fun message() = message
+}
