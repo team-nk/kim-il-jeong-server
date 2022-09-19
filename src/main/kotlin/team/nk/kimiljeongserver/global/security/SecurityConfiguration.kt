9d7a3e9aa513f9468ad0405eee9ef8f7f05e2ba5
Jeong Yoon <dlwjddbs14@naver.com>
2022-09-19 10:40:17 +0900

    feat: security configuration

---
+++ src/main/kotlin/team/nk/kimiljeongserver/global/security/SecurityConfiguration.kt
@@ -0,0 +1,31 @@
+package team.nk.kimiljeongserver.global.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfiguration {
+
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+            .cors().and()
+
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        http
+            .authorizeHttpRequests()
+            .antMatchers("*").permitAll()
+
+        return http.build()
+    }
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
+}
