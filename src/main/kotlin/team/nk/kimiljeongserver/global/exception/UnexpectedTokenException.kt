80160c3b058efb3e30660ab5b0183b5486b295d9
jaemin <sonjm3518@dsm.hs.kr>
2022-09-29 16:40:12 +0900

    📑 :: UnexpectedTokenException

---
+++ src/main/kotlin/team/nk/kimiljeongserver/global/exception/UnexpectedTokenException.kt
@@ -0,0 +1,11 @@
+package team.nk.kimiljeongserver.global.exception
+
+import team.nk.kimiljeongserver.global.error.GlobalErrorCode
+import team.nk.kimiljeongserver.global.error.exception.KimIlJeongException
+
+object UnexpectedTokenException : KimIlJeongException(
+    GlobalErrorCode.UNEXPECTED_TOKEN
+) {
+    @JvmField
+    val EXCEPTION = UnexpectedTokenException
+}
