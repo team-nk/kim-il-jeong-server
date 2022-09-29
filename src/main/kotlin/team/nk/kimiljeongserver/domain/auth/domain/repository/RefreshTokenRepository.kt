75f97b3409059167b29888fe8abc824bbd152544
jaemin <sonjm3518@dsm.hs.kr>
2022-09-29 16:55:20 +0900

    ⚡ :: Repository annotation 추가

--- src/main/kotlin/team/nk/kimiljeongserver/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -1,6 +1,8 @@
 package team.nk.kimiljeongserver.domain.auth.domain.repository
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 import team.nk.kimiljeongserver.domain.auth.domain.RefreshToken
 
+@Repository
 interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
