9767b97409fd10c4f9c4e54c0a1314136e9222f1
jaemin <sonjm3518@dsm.hs.kr>
2022-10-18 10:50:21 +0900

    feat: AwsSESConfig

---
+++ src/main/kotlin/team/nk/kimiljeongserver/global/config/AwsSESConfig.kt
@@ -0,0 +1,27 @@
+package team.nk.kimiljeongserver.global.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsync
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import team.nk.kimiljeongserver.infrastructure.aws.email.AwsSESProperties
+
+@Configuration
+class AwsSESConfig(
+    val awsSESProperties: AwsSESProperties
+) {
+    @Bean
+    fun amazonSimpleEmailService(): AmazonSimpleEmailServiceAsync {
+        val credentials = BasicAWSCredentials(
+            awsSESProperties.accessKey,
+            awsSESProperties.secretKey
+        )
+
+        return AmazonSimpleEmailServiceAsyncClient.asyncBuilder()
+            .withCredentials(AWSStaticCredentialsProvider(credentials))
+            .withRegion(awsSESProperties.region)
+            .build()
+    }
+}
