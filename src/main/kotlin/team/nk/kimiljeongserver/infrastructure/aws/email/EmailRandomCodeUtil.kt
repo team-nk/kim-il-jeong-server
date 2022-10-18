6813cb730c3f913f46f57202c94dd0e331eaa785
jaemin <sonjm3518@dsm.hs.kr>
2022-10-18 17:11:41 +0900

    refactor: random 로직 변경

--- src/main/kotlin/team/nk/kimiljeongserver/infrastructure/aws/email/EmailRandomCodeUtil.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/infrastructure/aws/email/EmailRandomCodeUtil.kt
@@ -1,8 +1,32 @@
 package team.nk.kimiljeongserver.infrastructure.aws.email
 
-class EmailRandomCodeUtil {
+import java.security.SecureRandom
 
-    fun generateCode(): Int {
-        return (1000..9999).random()
+
+class EmailRandomCodeUtil(
+    private val random: SecureRandom
+) {
+
+    fun generateCode(): String {
+        val randomList = ArrayList<Char>()
+        var codeList = ""
+
+        for (n in '0'..'9') {
+            randomList.add(n)
+        }
+
+        for (n in 'a'..'z') {
+            randomList.add(n)
+        }
+
+        for (n in 'A'..'Z') {
+            randomList.add(n)
+        }
+
+        for (i in 1..6) {
+            codeList += randomList[random.nextInt(randomList.size)]
+        }
+
+        return codeList
     }
 }
