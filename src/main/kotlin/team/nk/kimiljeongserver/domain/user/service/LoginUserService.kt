963541db2b2c19be9335bfe745c6755f9bd6fda7
jaemin <sonjm3518@dsm.hs.kr>
2022-10-19 15:33:42 +0900

    feat: LoginUserService

---
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/user/service/LoginUserService.kt
@@ -0,0 +1,31 @@
+package team.nk.kimiljeongserver.domain.user.service
+
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import team.nk.kimiljeongserver.domain.auth.presentation.dto.response.TokenResponse
+import team.nk.kimiljeongserver.domain.user.exception.PasswordMissMatchedException
+import team.nk.kimiljeongserver.domain.user.facade.UserFacade
+import team.nk.kimiljeongserver.domain.user.presentation.dto.request.LoginUserRequest
+import team.nk.kimiljeongserver.global.security.jwt.JwtTokenProvider
+
+@Service
+class LoginUserService(
+    private val userFacade: UserFacade,
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+
+    @Transactional
+    fun execute(loginUserRequest: LoginUserRequest): TokenResponse {
+        val user = userFacade.getUserByEmail(loginUserRequest.email)
+
+        if (!passwordEncoder.matches(loginUserRequest.password, user.password)) {
+            throw PasswordMissMatchedException.EXCEPTION
+        }
+
+        val token =  jwtTokenProvider.getToken(loginUserRequest.email)
+
+        return TokenResponse(token.accessToken, token.refreshToken)
+    }
+}
