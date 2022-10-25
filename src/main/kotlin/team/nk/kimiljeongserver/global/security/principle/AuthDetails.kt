4b6a7b66d657d5747683f0cc5d932e41299210c5
jaemin <sonjm3518@dsm.hs.kr>
2022-10-25 23:58:47 +0900

    feat: user 넘기기

--- src/main/kotlin/team/nk/kimiljeongserver/global/security/principle/AuthDetails.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/global/security/principle/AuthDetails.kt
@@ -3,16 +3,17 @@ package team.nk.kimiljeongserver.global.security.principle
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import team.nk.kimiljeongserver.domain.user.domain.User
 
-class AuthDetails : UserDetails {
+class AuthDetails(
+    private val user: User
+) : UserDetails {
 
-    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
-        return mutableListOf(SimpleGrantedAuthority(TODO("ROLE")))
-    }
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> = mutableListOf()
 
     override fun getPassword(): String? = null
 
-    override fun getUsername(): String = TODO("USER_NAME")
+    override fun getUsername(): String = user.email
 
     override fun isAccountNonExpired(): Boolean = true
 
