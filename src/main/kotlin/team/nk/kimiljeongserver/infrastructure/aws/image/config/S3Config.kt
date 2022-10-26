f144911edafd85b9bdc2f2da0c763ff155132b0c
jaemin <sonjm3518@dsm.hs.kr>
2022-10-26 20:05:13 +0900

    feat: ImageConfig

---
+++ src/main/kotlin/team/nk/kimiljeongserver/infrastructure/aws/image/config/S3Config.kt
@@ -0,0 +1,31 @@
+package team.nk.kimiljeongserver.infrastructure.aws.image.config
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Config(
+    @Value("\${cloud.aws.credentials.access-key}")
+    private var accessKey: String,
+
+    @Value("\${cloud.aws.credentials.secret-key}")
+    private val secretKey: String,
+
+    @Value("\${cloud.aws.region.static}")
+    private val region: String
+) {
+
+    @Bean
+    fun amazonS3Client(): AmazonS3Client {
+        val awsCredentials = BasicAWSCredentials(accessKey, secretKey)
+        return AmazonS3ClientBuilder.standard()
+            .withRegion(region)
+            .withCredentials(AWSStaticCredentialsProvider(awsCredentials))
+            .build() as AmazonS3Client
+    }
+}
