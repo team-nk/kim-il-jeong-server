02ce97499b03988c5571007c48d3055e13e393c4
jaemin <sonjm3518@dsm.hs.kr>
2022-10-29 22:28:38 +0900

    feat: Querydsl setting

---
+++ src/main/kotlin/team/nk/kimiljeongserver/global/config/QuerydslConfig.kt
@@ -0,0 +1,14 @@
+package team.nk.kimiljeongserver.global.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+
+@Configuration
+class QuerydslConfig(
+    private val entityManager: EntityManager
+) {
+    @Bean
+    fun jpaQueryFactory() = JPAQueryFactory(entityManager)
+}
