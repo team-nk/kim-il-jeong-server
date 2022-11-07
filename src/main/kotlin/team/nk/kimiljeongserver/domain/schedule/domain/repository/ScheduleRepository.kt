1f54bcbbb6dfdd6357495ca24f0e952dac301c21
jaemin <sonjm3518@dsm.hs.kr>
2022-11-07 15:12:24 +0900

    feat: findAllSchedule

--- src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/domain/repository/ScheduleRepository.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/domain/repository/ScheduleRepository.kt
@@ -1,9 +1,12 @@
 package team.nk.kimiljeongserver.domain.schedule.domain.repository
 
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import team.nk.kimiljeongserver.domain.schedule.domain.Schedule
 import team.nk.kimiljeongserver.domain.user.domain.User
+import java.math.BigDecimal
 
 @Repository
 interface ScheduleRepository : CrudRepository<Schedule, Int> {
@@ -14,4 +17,11 @@ interface ScheduleRepository : CrudRepository<Schedule, Int> {
 
     fun findAllByUserOrderByStartTimeDesc(user: User): List<Schedule>?
 
+    @Query(
+        value = "SELECT *, (6371 * acos(cos(radians(:x)) * cos(radians(s.latitude)) * cos(radians(s.longitude) - radians(:y)) + sin(radians(:x)) * sin(radians(s.latitude)))) AS distance FROM tbl_schedule AS s HAVING distance <= :d ORDER BY distance;",
+        nativeQuery = true
+    )
+    fun findAllSchedule(
+        @Param(value = "x") x: BigDecimal, @Param(value = "y") y: BigDecimal, @Param(value = "d") d: BigDecimal
+    ): List<Schedule>
 }
