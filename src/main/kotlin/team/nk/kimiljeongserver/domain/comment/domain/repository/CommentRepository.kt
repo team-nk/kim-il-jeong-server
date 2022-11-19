42bbc409ced1989cb3186d9bcc3dad58da1fa5b4
jaemin <sonjm3518@dsm.hs.kr>
2022-11-19 23:59:15 +0900

    feat: count

--- src/main/kotlin/team/nk/kimiljeongserver/domain/comment/domain/repository/CommentRepository.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/comment/domain/repository/CommentRepository.kt
@@ -2,6 +2,8 @@ package team.nk.kimiljeongserver.domain.comment.domain.repository
 
 import org.springframework.data.repository.CrudRepository
 import team.nk.kimiljeongserver.domain.comment.domain.Comment
+import team.nk.kimiljeongserver.domain.post.domain.Post
 
 interface CommentRepository : CrudRepository<Comment, Int>, CustomCommentRepository {
+    fun countAllByPost(post: Post): Int
 }
