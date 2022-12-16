d176745a7d7f3a08cf8e169746a880826ed76a8f
jaemin <sonjm3518@dsm.hs.kr>
2022-12-16 14:15:37 +0900

    feat: test 관련 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,32 @@ repositories {
 	mavenCentral()
 }
 
-group = "team.nk"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+allprojects {
+	group = "team.nk"
+	version = "0.0.1-SNAPSHOT"
 
-repositories {
-	mavenCentral()
+	apply(plugin = "jacoco")
+
+	tasks {
+		compileKotlin {
+			kotlinOptions {
+				freeCompilerArgs = listOf("-Xjsr305=strict")
+				jvmTarget = "17"
+			}
+		}
+
+		compileJava {
+			sourceCompatibility = JavaVersion.VERSION_17.majorVersion
+		}
+
+		test {
+			useJUnitPlatform()
+		}
+	}
+
+	repositories {
+		mavenCentral()
+	}
 }
 
 dependencyManagement {
@@ -34,27 +54,56 @@ dependencyManagement {
 }
 
 dependencies {
+	//jpa
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+	//jwt
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
+
+	//redis
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	//security
 	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	//validation
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
+	//web
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	//kotlin
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	//mail
 	implementation ("org.springframework.boot:spring-boot-starter-mail")
+
+	//ses
 	implementation("com.amazonaws:aws-java-sdk-ses:1.12.315")
-	implementation("org.projectlombok:lombok:1.18.24")
+
+	//mysql
 	runtimeOnly("mysql:mysql-connector-java")
+
+	//java servlet
 	implementation("javax.servlet:javax.servlet-api:4.0.1")
+
+	//aws
 	implementation ("org.springframework.cloud:spring-cloud-starter-aws")
+
+	//querydsl
 	implementation("com.querydsl:querydsl-jpa:5.0.0")
 	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+
+	//openfeign
 	implementation ("org.springdoc:springdoc-openapi-ui:1.6.11")
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
+
+	//test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 }
 
 kotlin.sourceSets.main {
@@ -73,10 +122,23 @@ allOpen {
 	annotation("javax.persistence.Embeddable")
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "17"
+tasks.register<JacocoReport>("jacocoRootReport") {
+	subprojects {
+		this@subprojects.plugins.withType<JacocoPlugin>().configureEach {
+			this@subprojects.tasks.matching {
+				it.extensions.findByType<JacocoTaskExtension>() != null
+			}
+				.configureEach {
+					sourceSets(this@subprojects.the<SourceSetContainer>().named("main").get())
+					executionData(this)
+				}
+		}
+	}
+
+	reports {
+		xml.outputLocation.set(File("${buildDir}/reports/jacoco/test/jacocoTestReport.xml"))
+		xml.required.set(true)
+		html.required.set(false)
 	}
 }
 
