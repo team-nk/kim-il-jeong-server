795dddc567440f5757259228769e44e8a29efea6
jaemin <sonjm3518@dsm.hs.kr>
2022-12-30 10:46:18 +0900

    feat: AuthController

---
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/auth/presentation/AuthController.kt
@@ -0,0 +1,23 @@
+package team.nk.kimiljeongserver.domain.auth.presentation
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import team.nk.kimiljeongserver.domain.auth.presentation.dto.response.TokenResponse
+import team.nk.kimiljeongserver.domain.auth.service.ReissueService
+
+@Tag(name = "인증", description = "인증 관련 API입니다.")
+@RequestMapping("/auth")
+@RestController
+class AuthController(
+    private val reissueService: ReissueService
+) {
+
+    @Operation(summary = "댓글 작성")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PutMapping
+    fun createComment(@RequestHeader("Refresh-Token") refreshToken: String): TokenResponse {
+        return reissueService.execute(refreshToken)
+    }
+}
