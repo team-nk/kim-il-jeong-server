c3cfdbf22a487dcb8b26925992a77f2bb2430811
jaemin <sonjm3518@dsm.hs.kr>
2023-01-02 22:28:46 +0900

    chore: validate 제거

--- src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyBirthdayRequest.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyBirthdayRequest.kt
@@ -1,10 +1,9 @@
 package team.nk.kimiljeongserver.domain.user.presentation.dto.request
 
 import java.time.LocalDate
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 data class ModifyBirthdayRequest(
 
-    @field: NotBlank(message = "birthday는 필수 입력입니다.")
     val birthday: LocalDate
 )
