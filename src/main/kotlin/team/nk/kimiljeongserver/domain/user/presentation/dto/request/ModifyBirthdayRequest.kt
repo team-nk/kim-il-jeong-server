ca2997fb4aa3fe98e01fdea771c31814fbcdee07
jaemin <sonjm3518@dsm.hs.kr>
2023-01-02 10:30:37 +0900

    refactor: validate 추가

--- src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyBirthdayRequest.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyBirthdayRequest.kt
@@ -1,7 +1,10 @@
 package team.nk.kimiljeongserver.domain.user.presentation.dto.request
 
 import java.time.LocalDate
+import javax.validation.constraints.NotBlank
 
 data class ModifyBirthdayRequest(
+
+    @field: NotBlank(message = "birthday는 필수 입력입니다.")
     val birthday: LocalDate
 )
--- src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyPasswordRequest.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/user/presentation/dto/request/ModifyPasswordRequest.kt
@@ -1,7 +1,15 @@
 package team.nk.kimiljeongserver.domain.user.presentation.dto.request
 
+import javax.validation.constraints.NotBlank
+
 data class ModifyPasswordRequest(
+
+    @field: NotBlank(message = "password은 필수 입력입니다.")
     val nowPassword: String,
+
+    @field: NotBlank(message = "newPassword은 필수 입력입니다.")
     val newPassword: String,
+
+    @field: NotBlank(message = "newPassword2는 필수 입력입니다.")
     val new2Password: String
 )
