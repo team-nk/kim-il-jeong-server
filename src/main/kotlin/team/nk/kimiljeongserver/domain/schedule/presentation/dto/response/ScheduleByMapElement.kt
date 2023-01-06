7f9e44d6723ceb5857e20d3bd040879b38c631ea
jaemin <sonjm3518@dsm.hs.kr>
2023-01-06 17:25:13 +0900

    chore: 모든 일정 넘기기로 수정

--- src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/presentation/dto/response/ScheduleByMapElement.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/presentation/dto/response/ScheduleByMapElement.kt
@@ -1,10 +1,13 @@
 package team.nk.kimiljeongserver.domain.schedule.presentation.dto.response
 
 import team.nk.kimiljeongserver.domain.schedule.domain.type.Color
+import java.time.LocalDateTime
 
 data class ScheduleByMapElement(
     val scheduleId: Int,
     val content: String,
     val address: String,
-    val color: Color
+    val color: Color,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime
 )
--- src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/service/QueryScheduleByMapService.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/service/QueryScheduleByMapService.kt
@@ -2,13 +2,11 @@ package team.nk.kimiljeongserver.domain.schedule.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import team.nk.kimiljeongserver.domain.schedule.domain.Schedule
 import team.nk.kimiljeongserver.domain.schedule.domain.repository.ScheduleRepository
 import team.nk.kimiljeongserver.domain.schedule.presentation.dto.request.ScheduleByMapRequest
 import team.nk.kimiljeongserver.domain.schedule.presentation.dto.response.ScheduleByMapElement
 import team.nk.kimiljeongserver.domain.schedule.presentation.dto.response.ScheduleByMapListResponse
 import team.nk.kimiljeongserver.domain.user.facade.UserFacade
-import java.time.LocalDateTime
 
 @Service
 class QueryScheduleByMapService(
@@ -19,27 +17,31 @@ class QueryScheduleByMapService(
     @Transactional(readOnly = true)
     fun execute(request: ScheduleByMapRequest): ScheduleByMapListResponse {
         val user = userFacade.getCurrentUser()
-        val scheduleList = scheduleRepository.findAllSchedule(request.latitude, request.longitude, request.distance)
+        //val scheduleList = scheduleRepository.findAllSchedule(request.latitude, request.longitude, request.distance)
+        val scheduleList = scheduleRepository.findAll()
             .filter {
-                isToday(it) && it.user == user
+                //isToday &&
+                it.user == user
             }
             .map {
                 ScheduleByMapElement(
                     scheduleId = it.id,
                     content = it.content,
                     address = it.address,
-                    color = it.color
+                    color = it.color,
+                    startTime = it.startTime,
+                    endTime = it.endTime
                 )
             }
 
         return ScheduleByMapListResponse(scheduleList)
     }
-
+/*
     private fun isToday(schedule: Schedule): Boolean {
         val date = LocalDateTime.now()
         val startTime = schedule.startTime
         val tomorrow = date.plusDays(1)
 
         return startTime.isAfter(date) && startTime.isBefore(tomorrow)
-    }
+    }*/
 }
