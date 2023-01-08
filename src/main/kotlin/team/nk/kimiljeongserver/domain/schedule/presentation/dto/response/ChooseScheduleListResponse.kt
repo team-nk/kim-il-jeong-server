c6cb3fe2c456f43fb67e551d9be396808b9fc2d2
jaemin <sonjm3518@dsm.hs.kr>
2023-01-08 14:34:12 +0900

    refactor: response 명 통일

--- src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/presentation/dto/response/ChooseScheduleListResponse.kt
+++ src/main/kotlin/team/nk/kimiljeongserver/domain/schedule/presentation/dto/response/ChooseScheduleListResponse.kt
@@ -1,5 +1,5 @@
 package team.nk.kimiljeongserver.domain.schedule.presentation.dto.response
 
 data class ChooseScheduleListResponse(
-    val chooseScheduleList: List<ChooseScheduleElement>?
+    val scheduleList: List<ChooseScheduleElement>?
 )
