ff21114c1d17f1ce1097f6493c031747440ea167
jaemin <sonjm3518@dsm.hs.kr>
2023-01-13 20:32:30 +0900

    delete: 불필요한 class 제거

--- src/main/kotlin/team/nk/kimiljeongserver/global/config/FeignClientsConfig.kt
+++
@@ -1,9 +0,0 @@
-package team.nk.kimiljeongserver.global.config
-
-import org.springframework.cloud.openfeign.EnableFeignClients
-import org.springframework.context.annotation.Configuration
-
-@EnableFeignClients
-@Configuration
-class FeignClientsConfig {
-}
